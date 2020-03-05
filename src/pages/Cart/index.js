1248d6dbb03e90f17cd02cea96eb4075492e9b3e
Thiago Pereira <thiagorpereira7@gmail.com>
2020-03-04 22:56:51 -0300

    Apply CSS and HTML of Cart page

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,66 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>Produto</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://imgcentauro-a.akamaihd.net/250x250/93556002A1/tenis-fila-lugano-6-0-masculino-img.jpg"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>Tenis muito massa</strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$259,80</strong>
+            </td>
+            <button type="button">
+              <MdDelete size={20} color="#7159c1" />
+            </button>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1540,56</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
